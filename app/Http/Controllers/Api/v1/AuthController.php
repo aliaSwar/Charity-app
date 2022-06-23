68de8660180923bccc9ebfb8bedb6770c8795d7c
alia swar <78238958+aliaSwar@users.noreply.github.com>
2022-06-23 17:21:49 +0300

    login and logout api

---
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    /**
+     * login user
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'name'     => 'required|string',
+            'password'  => 'required|string',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // he is a real
+
+            $user = $request->user();
+
+            $token = $user->createToken('auth')->plainTextToken;
+
+            $response = [
+                'message' => 'User successfully login',
+                'user'      =>  $user,
+                'token'     =>  $token
+            ];
+            return response($response, 201);
+        }
+    }
+
+    /**
+     * Logout user
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'has Logout'
+        ];
+    }
+}

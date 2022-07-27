6f5a91d7403f51deedb9613dc9be6c53cd499cb0
alia swar <78238958+aliaSwar@users.noreply.github.com>
2022-07-27 20:17:46 +0300

    update post url

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\v1\AuthController;
-use App\Http\Controllers\Api\V1\PostController;
+use App\Http\Controllers\Api\v1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
